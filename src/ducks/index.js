3e77f4a71651fbd73bb4f86f08c8ef158a3dc4b2
tu4mo <tu4mo@users.noreply.github.com>
2020-09-13 20:12:41 +0300

    Remove redux-form

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux'
-import { reducer as form } from 'redux-form'
 
-const reducers = combineReducers({
-  form
-})
+const reducers = combineReducers({})
 
 export default reducers
